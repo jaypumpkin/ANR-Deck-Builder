08e9f47c6d81faad50aebc247ba7adb815b3fea8
jaypumpkin <86137056+jaypumpkin@users.noreply.github.com>
2025-11-26 23:18:23 -0500

    Update JSON URLs to new GitHub Pages location

--- app/src/main/java/com/shuneault/netrunnerdeckbuilder/game/NetRunnerBD.java
+++ app/src/main/java/com/shuneault/netrunnerdeckbuilder/game/NetRunnerBD.java
@@ -12,9 +12,9 @@ public class NetRunnerBD {
 //    public static final String URL_CARDS_JSON = "https://anrdigital.github.io/ANR-Deck-Builder/app/src/main/res/raw/cardsv2.json";
 //    public static final String URL_PACKS_JSON = "https://anrdigital.github.io/ANR-Deck-Builder/app/src/main/res/raw/packs.json";
 //    public static final String URL_MWL_JSON = "https://anrdigital.github.io/ANR-Deck-Builder/app/src/main/res/raw/mwl.json";
-    public static final String URL_CYCLES_JSON = "https://anrdigital.github.io/ANR-Deck-Builder/app/src/main/res/raw/cycles.json";
-    public static final String URL_FORMATS_JSON = "https://anrdigital.github.io/ANR-Deck-Builder/app/src/main/res/raw/formats.json";
-    public static final String URL_ROTATIONS_JSON = "https://anrdigital.github.io/ANR-Deck-Builder/app/src/main/res/raw/rotations.json";
+    public static final String URL_CYCLES_JSON = "https://jaypumpkin.github.io/ANR-Deck-Builder/app/src/main/res/raw/cycles.json";
+    public static final String URL_FORMATS_JSON = "https://jaypumpkin.github.io/ANR-Deck-Builder/app/src/main/res/raw/formats.json";
+    public static final String URL_ROTATIONS_JSON = "https://jaypumpkin.github.io/ANR-Deck-Builder/app/src/main/res/raw/rotations.json";
 
     public static String getAllCardsUrl(String locale) {
         return String.format(URL_GET_ALL_CARDS, locale);
